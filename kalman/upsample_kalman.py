3da8bfc1f4372d4e83a97644e97d786c871fef12
kikass13 <nick.fiege@ducktrain.io>
2023-06-26 12:27:55 +0200

    upsamle kalman

--- kalman/upsample_kalman.py
+++ kalman/upsample_kalman.py
@@ -37,39 +37,50 @@ def example():
     ### x, vx, ax
     # F = np.array([[1, outputDt, 0.5*sq], [0, 1, outputDt], [0, 0, 1]])
     ### do not apply acceleration onto position .. for reasons
-    F = np.array([[1, outputDt, 0.0], [0, 1, outputDt], [0, 0, 1]])
-    H = np.array([[1, 0, 0], [0, 0, 0], [0, 0, 0]])
-    Q = np.array([[0.00001, 0.0, 0.0], [0.0, 5.0, 0.0], [0.0, 0.0, 10.0]])
-    R = np.array([[10.0, 0.0, 0.0], [0.0, 10000.0, 0.0], [0.0, 0.0, 10000.0]])
+    F = np.array([[1, outputDt], [0, 1]])
+    H = np.array([[1, 0], [0, 1]])
+    Q = np.array([[0.01, 0.0], [0.0, 1.0]])
+    R = np.array([[100.0, 0.0], [0.0, 0.1]])
 
     kf = KalmanFilter(F = F, H = H, Q = Q, R = R)
 
-    t = 0
+    t = 0.1
     measurements = []
     measurementTimes = []
-    lastMeasurement = None
+    lastMeasurement = 0
     states = []
+    states_v = []
     stateTimes = []
     lastState = None
     stepSample = 0.01
+    z_old = 0
+
+    comp_filter_value = 0.95
+    comp_filter_out = [0]
     while t < 10.0:
         if lastMeasurement == None or t - lastMeasurement >= inputDt:
-            lastMeasurement = t
             ### new input measurement
-            z = math .pi * math.sin(t*5.0)
+            z = math .pi * math.sin(t*2.0)
             noise = np.random.normal(0,0.5,1)[0]
-            z = z + noise
+            z = z + noise/2.0
             measurements.append(z)
             measurementTimes.append(t)
-            zM = np.array([z, 0, 0]) ### x,vx, ax
+            #print((z-z_old)/(t-lastMeasurement))
+            zM = np.array([0, (z-z_old)/(t-lastMeasurement)]) ### x,vx, ax
+            z_old = z
             kf.update(zM)
-        if lastState == None or t - lastState > outputDt:
+            lastMeasurement = t
+        elif lastState == None or t - lastState > outputDt:
             lastState = t
             s = kf.predict()
             print(s)
             print("===")
             states.append(s[0])
+            states_v.append(s[1]/10.0)
             stateTimes.append(t)
+
+            comp_in = measurements[-1]
+            comp_filter_out.append(comp_filter_out[-1] * comp_filter_value + comp_in* (1-comp_filter_value))
         plt.cla()
         # for stopping simulation with the esc key.
         plt.gcf().canvas.mpl_connect(
@@ -82,6 +93,9 @@ def example():
         #         plt.axvline(x=stamp,  ymin = val , ymax = 0)
         plt.step(measurementTimes, measurements, "-r", label="course")
         plt.step(stateTimes, states, "-b", label="course")
+        plt.step(stateTimes, states_v, "-y", label="course")
+
+        plt.step(stateTimes,comp_filter_out[1:], "-g", label="course")
 
         plt.axis("equal")
         plt.grid(True)
@@ -90,5 +104,8 @@ def example():
         t+=0.01
 
 
+import time
 if __name__ == '__main__':
     example()
+    #time.sleep(1000000)
+    plt.pause(100000)
